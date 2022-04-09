c7d1f0d3ba7f6499f5eac89ed4e006e006dfffef
Hain Thit <hainlthit@gmail.com>
2022-04-09 17:26:30 -0400

    sidebar + playlists

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,11 +1,12 @@
 import {Box} from '@chakra-ui/layout'
+import SideBar from './sideBar'
 
 const PlayerLayout = ({children}) => {
     return (
         
        <Box width="100vw" height="100vh">
            <Box position="absolute" top="0" width="250px" left="0">
-                SIDEBAR
+                <SideBar />
            </Box>
            <Box marginLeft="250px" marginBottom="100px">
             {children}
