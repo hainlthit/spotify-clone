7313db327ee95d995b02903a1365bf7e001cef9e
Hain Thit <hainlthit@gmail.com>
2022-04-09 15:55:04 -0400

    layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "reset-css";
+import PlayerLayout from "../components/playerLayout";
 
-const theme = extendTheme ({
+const theme = extendTheme({
   colors: {
     gray: {
-      100: '#F5f5f5',
-      200: '#EEEEEE',
-      300: '#E0E0E0',
-      400: '#BDBDBD',
-      500: '#9E9E9E',
-      600: '#757575',
-      700: '#616161',
-      800: '#424242',
-      900: '#212121'
+      100: "#F5f5f5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
     },
   },
   components: {
@@ -27,11 +28,16 @@ const theme = extendTheme ({
       },
     },
   },
-
-
 });
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <PlayerLayout>
+        <Component {...pageProps} />
+      </PlayerLayout>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
