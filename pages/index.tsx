2f624dfd1a58610fb8b4d4c92e4b740c901a822f
Hain Thit <hainlthit@gmail.com>
2022-05-26 11:01:10 -0400

    use seed data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,55 @@
+import { Box, Text, Flex } from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/react'
 import GradientLayout from '../components/gradientLayout'
+import { useMe } from '../lib/hooks'
+import prisma from '../lib/prisma'
 
+const Home = ({ artists }) => {
+  const { user } = useMe()
 
-const Home = () => {
-  return (<GradientLayout color="pink">
-    <div>Home Page</div>
-    </GradientLayout>)
+  return (
+    <GradientLayout
+      roundImage
+      color="pink"
+      subtitle="profile"
+      title="Hain Thit"
+      description="15 public playlists"
+      image="https://i.kym-cdn.com/entries/icons/original/000/040/241/anya_heh_thumbnail.jpg"
+    >
+      <Box color="white" paddingX="40px">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://placekitten.com/300/300"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
+    </GradientLayout>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({})
+
+  return {
+    props: { artists },
+  }
 }
 
 export default Home
